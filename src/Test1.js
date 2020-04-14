e88241ac2629f3c75e28fdbb0fa731361dfa423e
past13 <laurynas.janilionis@gmail.com>
2020-04-14 14:19:20 +0700

    clean first test form

--- src/Test1.js
+++ src/Test1.js
@@ -1,36 +1,50 @@
-const example = [
-    "AB1",
-    "AC4",
-    "AD10",
-    "BE3",
-    "CD4",
-    "CF2",
-    "DE1",
-    "EB3",
-    "EA2",
-    "FD1",
-  ];
-  const index = {};
-  example.forEach((e) => {
-    index[e[0]] = { ...index[e[0]], [e[1]]: +e.substr(2) };
-  });
+import React from 'react';
+
+const Test1 = () => {
   
-  const findRoute = (input) => {
-    let distance = 0;
-    for (var i = 1; i < input.length; i++) {
-      if (index[input[i - 1]][input[i]] === undefined) {
-        console.log(input, 'no such route')
-        return
-      }
-      distance += index[input[i - 1]][input[i]];
-    }
-    console.log('node', index[input])
-    return distance
+  const mapRef = React.useRef();
+  const destinationRef = React.useRef();
+
+  const mapExample = [
+        "AB1", 
+        "AC4", 
+        "AD10", 
+        "BE3", 
+        "CD4", 
+        "CF2", 
+        "DE1", 
+        "EB3", 
+        "EA2", 
+        "FD1",
+  ];
+
+  const destinExampl = ["A", "B", "E"];
+
+  const handleSubmit = () => {
+    
   };
 
-  // findRoute(["A", "B", "E"])
-  // findRoute(["A", "D"])
-  // findRoute(["E", "A", "C", "F"])
-  // findRoute(["A", "D", "F"])
-  // findRoute('E');
+  return (
+    <>
+      <label>
+        Full map
+        <input 
+        ref={mapRef}
+        defaultValue= {mapExample} />
+      </label>
+      <br />
+      <label>
+        Destination
+        <input ref={destinationRef} 
+        defaultValue= {destinExampl} />
+      </label>
+      <br />
+      <button onClick={handleSubmit}>Submit</button>
+    </>
+  );
+};
+
+export default Test1;
+
+
 
